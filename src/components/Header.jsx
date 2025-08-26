871b73506c897f10c73cd12e6241abd4843b5feb
Amaan Sheikh <amaansheikhbrothers@gmail.com>
2025-08-26 16:47:01 +0530

    new commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -30,7 +30,7 @@ export default function Header(){
       />
     </div>
   
- =
+ 
     <nav className="flex items-center justify-end gap-4">
       <Link to="/cart" className="relative">
         <button className="bg-blue-600 text-white px-4 py-2 rounded">
